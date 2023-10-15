f7ac735c644f5cca2473775b3cd8a85a5f6afa62
hoangnam <namzinv123@gmail.com>
2023-10-15 17:27:13 +0700

    create user update api

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
-import {app} from '../firebase'
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
     const { currentUser } = useSelector((state) => state.user);
     const fileRef = useRef(null);
-    const [file, setFile] = useState(undefined)
-    const [filePerc, setFilePerc] = useState(0)
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
     const [fileUploadError, setFileUploadError] = useState(false);
-    const [formData, setFormDate] = useState({})
+    const [formData, setFormDate] = useState({});
 
     // firebase storage
     // allow read;
@@ -18,37 +23,47 @@ export default function Profile() {
     // request.resource.contentType.matches('image/.*')
 
     useEffect(() => {
-        if(file) {
+        if (file) {
             handleFileUpload(file);
         }
-    }, [file])
+    }, [file]);
 
     const handleFileUpload = (file) => {
-        const storage = getStorage(app)
+        const storage = getStorage(app);
         const fileName = new Date().getTime() + file.name;
         const storageRef = ref(storage, fileName);
         const uploadTask = uploadBytesResumable(storageRef, file);
 
-        uploadTask.on('state_changed',
+        uploadTask.on(
+            "state_changed",
             (snapshot) => {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes * 100);
-                setFilePerc(Math.round(progress))
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFilePerc(Math.round(progress));
             },
             () => {
                 setFileUploadError(true);
             },
             () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => setFormDate({...formData, avatar: downloadURL}))
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+                    setFormDate({ ...formData, avatar: downloadURL })
+                );
             }
-        )
-    }
+        );
+    };
 
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
             <form className="flex flex-col gap-4">
-                <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
+                <input
+                    onChange={(e) => setFile(e.target.files[0])}
+                    type="file"
+                    ref={fileRef}
+                    hidden
+                    accept="image/*"
+                />
                 <img
                     className="w-24 h-24 rounded-full object-cover cursor-pointer self-center"
                     src={formData.avatar || currentUser.avatar}
@@ -57,11 +72,19 @@ export default function Profile() {
                 />
 
                 <p className="text-center">
-                    {fileUploadError ? (<span className="text-red-700">Error image upload (image must be less than 2 MB)</span>)
-                        : filePerc > 0 && filePerc < 100 ? (<span className="text-slate-700">{`Uploading ${filePerc}%`}</span>)
-                        : filePerc === 100 ? (<span className="text-green-700">Image successfully uploaded</span>)
-                        : ('')
-                    }
+                    {fileUploadError ? (
+                        <span className="text-red-700">
+                            Error image upload (image must be less than 2 MB)
+                        </span>
+                    ) : filePerc > 0 && filePerc < 100 ? (
+                        <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+                    ) : filePerc === 100 ? (
+                        <span className="text-green-700">
+                            Image successfully uploaded
+                        </span>
+                    ) : (
+                        ""
+                    )}
                 </p>
 
                 <input
