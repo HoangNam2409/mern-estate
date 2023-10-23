97b015f50290a6f6e5dbd155358a99bed6ed2122
hoangnam <namzinv123@gmail.com>
2023-10-23 11:42:08 +0700

    complete delete user listing functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,7 +4,6 @@ import {verifyToken} from '../utils/verifyUser.js'
 
 const router = express.Router();
 
-router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
