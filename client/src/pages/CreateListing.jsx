371517fed7f98bb05851fd36ed3d8b8f8d7c717d
hoangnam <namzinv123@gmail.com>
2023-10-28 09:15:44 +0700

    create search api route

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -297,7 +297,7 @@ export default function CreateListing() {
                             />
                             <div className="flex flex-col items-center">
                                 <p>Regular price</p>
-                                <span className="text-xs">($ / month)</span>
+                                {formData.type === 'rent' && <span className="text-xs">($ / month)</span>}
                             </div>
                         </div>
 
@@ -314,7 +314,7 @@ export default function CreateListing() {
                                 />
                                 <div className="flex flex-col items-center">
                                     <p>Discount price</p>
-                                    <span className="text-xs">($ / month)</span>
+                                    {formData.type === 'rent' && <span className="text-xs">($ / month)</span>}
                                 </div>
                             </div>
                         )}
